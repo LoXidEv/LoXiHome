9293cbbf039a5f2fc755791ffcb95e73521ca371
洛溪 LoXi <yuansepailin@163.com>
2025-09-12 19:15:47 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@ import COSH from '@/components/COSH.vue';
         <div class="loxi_title">Hello friend.</div>
         <div class="loxi_subtitle">Welcome to my website!</div>
         <div>
+            <img href="https://count.getloli.com" width="100%" alt="Moe Counter" src="https://count.getloli.com/@LoXidEv?name=LoXidEv&theme=booru-ve&padding=7&offset=0&align=top&scale=1&pixelated=1&darkmode=auto">
             你好！我是洛溪，英文名 LoXi，开发时常用 LoXidEv，是一名来自中国的学生，计算机开发爱好者。
             <br>
             自 2020 年开启自学之旅以来，我在代码的世界里不断探索成长，如今已积累了不少实践经验。
